a670b1f0ed6009818c1c4a1a72d0d81e1d361985
jorgelucasalima <jorgelucasalima@gmail.com>
2022-12-01 16:50:30 -0300

    ajustado a conexão com api

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,24 +1,44 @@
 import { useState } from 'react';
 import { Container, Content } from './styles';
-import api from './../../services/api';
+import api from './../../services/api'
 
 export default function Button() {
 
   const [cepDigitado, setCepDigitado] = useState('')
+  const [cepSalvo, setCepSalvo] = useState([])
 
-  //o axios é uma promisse então tive que usar o await e botar a função como asyncrona
-  async function getCep() {
-    const retorno = await api.get(`50980360/json/`)
-    console.log(retorno)
+
+  //Função de pegar o cep via api
+  async function getCep(event) {
+    event.preventDefault()
+  
+    const response = await api.get(`${cepDigitado}/json/`)
+    .then((response) => {
+      console.log('o response é:' , response)
+      setCepSalvo(response.data)
+    })
+    .catch((error) => {
+      console.log('o erro: ', error)
+    })
   }
 
+  console.log('CEP SALVO: ', cepSalvo)
+  //console.log('CEP DIGITADO: ', cepDigitado)
+
+
   return(
-    <Container>
-      <Content>
-        <input type="text" placeholder="CEP" value={cepDigitado}/>
-        <button type='submit' onClick={getCep}>Buscar</button>
-      </Content>  
-    </Container>
-    
+      <Container>
+        <Content>
+          <form onClick={getCep}>
+            <input 
+              type="text" 
+              placeholder="CEP"
+              value={cepDigitado}
+              onChange={(event) => setCepDigitado(event.target.value)}
+            />
+            <button type='submit'>Buscar</button>
+          </form>
+        </Content>  
+      </Container>
   )
 }
