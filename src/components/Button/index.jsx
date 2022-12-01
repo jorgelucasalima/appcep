e6c81b2bd8fef3a58fcb14f55e30f7ab8b4f4fff
jorgelucasalima <jorgelucasalima@gmail.com>
2022-11-30 22:29:12 -0300

    implementando comunicação com api

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import { Container, Content } from './styles';
+import api from './../../services/api';
 
 export default function Button() {
+
+  const [cepDigitado, setCepDigitado] = useState('')
+
+  //o axios é uma promisse então tive que usar o await e botar a função como asyncrona
+  async function getCep() {
+    const retorno = await api.get(`50980360/json/`)
+    console.log(retorno)
+  }
+
   return(
     <Container>
       <Content>
-        <input type="text" placeholder="CEP" />
-        <button>Buscar</button>
+        <input type="text" placeholder="CEP" value={cepDigitado}/>
+        <button type='submit' onClick={getCep}>Buscar</button>
       </Content>  
     </Container>
     
